0c489e209b52a957a27c87e3387d6e2771ea0648
Archit Chandra <architchandra@gmail.com>
2023-01-01 15:46:23 +0530

    Set up types for custom app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,7 @@
-export default function App({ Component, pageProps }) {
+import { AppProps } from 'next/app';
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
