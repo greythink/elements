40f9117024a8f6ad0d67dff922750f2cbbbd6db3
Archit Chandra <architchandra@gmail.com>
2023-01-02 10:03:33 +0530

    Display logo on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,17 @@ import logo from '../public/media/element_school_logo_2000w.png';
 
 export default function Home() {
     return (
-        <Image
-            src={logo}
-            width={800}
-            alt="The Elements School logo"
-        />
+        <section className="container mt-24 flex flex-col items-center">
+            <h1 className="w-1/3">
+                <Image
+                    src={logo}
+                    width={800}
+                    alt="The Elements School logo"
+                />
+                <span className="sr-only">
+                    The Elements School
+                </span>
+            </h1>
+        </section>
     );
 }
--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+
+
 module.exports = {
-  content: [
-    "./app/**/*.tsx",
-    "./pages/**/*.tsx",
-    "./components/**/*.tsx",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './app/**/*.tsx',
+        './pages/**/*.tsx',
+        './components/**/*.tsx',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1.5rem',
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
 }
