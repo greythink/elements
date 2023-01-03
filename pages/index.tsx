6240c59599504021d41f50bc8e9464995b24c0ae
Archit Chandra <architchandra@gmail.com>
2023-01-03 19:39:24 +0530

    Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import logo from '../public/media/element_school_logo_2000w.png';
 
@@ -5,31 +6,36 @@ import logo from '../public/media/element_school_logo_2000w.png';
 
 export default function Home() {
     return (
-        <section>
-            <header className="flex flex-col items-center">
-                <h1 className="xs:w-5/6 sm:w-2/3 md:w-1/2 lg:w-1/3">
-                    <Image
-                        src={logo}
-                        width={800}
-                        alt="The Elements School logo"
-                    />
-                    <span className="sr-only">
-                        The Elements School
-                    </span>
-                </h1>
-            </header>
-            <section className="max-w-prose mx-auto text-center">
-                <h2 className="mt-20 text-2xl sm:text-3xl text-brand-red leading-relaxed">
-                    <span className="font-bold uppercase">
-                        Admissions Open
-                    </span>
-                    <br />
-                    Pre-Nursery, Nursery, <span className="break-keep">L-KG</span> and <span className="break-keep">U-KG</span>
-                </h2>
-                <h2 className="mt-20 text-2xl">
-                    Website Coming Soon
-                </h2>
+        <>
+            <Head>
+                <title>The Elements School - Admissions Open</title>
+            </Head>
+            <section>
+                <header className="flex flex-col items-center">
+                    <h1 className="xs:w-5/6 sm:w-2/3 md:w-1/2 lg:w-1/3">
+                        <Image
+                            src={logo}
+                            width={800}
+                            alt="The Elements School logo"
+                        />
+                        <span className="sr-only">
+                            The Elements School
+                        </span>
+                    </h1>
+                </header>
+                <section className="max-w-prose mx-auto text-center">
+                    <h2 className="mt-20 text-2xl sm:text-3xl text-brand-red leading-relaxed">
+                        <span className="font-bold uppercase">
+                            Admissions Open
+                        </span>
+                        <br />
+                        Pre-Nursery, Nursery, <span className="break-keep">L-KG</span> and <span className="break-keep">U-KG</span>
+                    </h2>
+                    <h2 className="mt-20 text-2xl">
+                        Website Coming Soon
+                    </h2>
+                </section>
             </section>
-        </section>
+        </>
     );
 }
